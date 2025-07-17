6216e3894c2258f0915c5a8dea696ae8c055c4db
JogendraDev30 <157616093+JogendraDev30@users.noreply.github.com>
2025-07-17 20:54:08 +0530

    Update 20250708160447_add_delivery_person_id_to_delivery_assignments.rb
    Removed duplicate column

--- db/migrate/20250708160447_add_delivery_person_id_to_delivery_assignments.rb
+++ db/migrate/20250708160447_add_delivery_person_id_to_delivery_assignments.rb
@@ -1,5 +1,5 @@
 class AddDeliveryPersonIdToDeliveryAssignments < ActiveRecord::Migration[7.1]
   def change
-    add_column :delivery_assignments, :delivery_person_id, :integer
+    #add_column :delivery_assignments, :delivery_person_id, :integer
   end
 end
