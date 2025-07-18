a5e6386c23e7351b3c4c086243c8dcbd5710efc8
Jogendra Majhi <jogendra.majhi@letsventure.com>
2025-07-18 09:03:44 +0530

    added free domain

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,12 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+   # Add this line
+   config.hosts << "atmanirbharfarm.work.gd"
+
+   # (Optional) add www subdomain if needed
+   config.hosts << "www.atmanirbharfarm.work.gd"
+
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
