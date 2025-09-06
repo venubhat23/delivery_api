5f10d69b2ffe2cba71ed1d7bb661c8af42424349
venu bhat <venubha147@gmail.com>
2025-09-06 08:35:48 +0530

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
     "gnu-modern-totally.ngrok-free.app",
     "atmanirbharfarm.work.gd",
     "13.232.153.224",                # Allow requests from the production server IP
-    "https://steady-raccoon-enormous.ngrok-free.app"
+    "steady-raccoon-enormous.ngrok-free.app"
   ].compact
     config.hosts << "localhost:3002"
 
